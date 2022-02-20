641102b395317ee81d436cdcebfbe7252ba1e641
Di <wi.ar.venom@gmail.com>
2022-02-21 01:43:20 +0300

    finish

--- HomeWork_lesson1/src/main/java/HomeWorkApp.java
+++ HomeWork_lesson1/src/main/java/HomeWorkApp.java
@@ -1,3 +1,48 @@
 public class HomeWorkApp {
-    
+
+    public static void main(String[] args) {
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+    }
+
+    public static void printThreeWords() {
+        System.out.println("Orange\nBanana\nApple");
+    }
+
+    public static void checkSumSign() {
+        int a = 7;
+        int b = 0;
+
+        if(a + b >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+
+    public static void printColor() {
+        int value = 100;
+
+        if (value <= 0) {
+            System.out.println("Красный");
+        } else if(value > 100) {
+            System.out.println("Зеленый");
+        } else {
+            System.out.println("Желтый");
+        }
+    }
+
+    public static void compareNumbers() {
+        int a = 200;
+        int b = 100;
+
+        if(a >= b) {
+            System.out.println("a >= b");
+        } else {
+            System.out.println("a < b");
+        }
+    }
+
 }
